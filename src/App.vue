bcbea28da7c0d4d75d723b599785971007b8bacf
slowgenius <305109030@qq.com>
2020-03-10 16:35:32 +0800

    show articles

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <div id="app">
-    <home/>
-  </div>
+    <div id="app">
+        <my-head/>
+        <home/>
+    </div>
 </template>
 
 <script>
-import home from './components/home.vue'
+    import home from './views/home.vue'
+    import MyHead from './components/myhead.vue'
 
-export default {
-  name: 'App',
-  components: {
-    home
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            home, MyHead
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #2c3e50;
+    }
 </style>
---
+++ src/components/myhead.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="head">
+        <el-row class="head-el">
+            <el-col :span="6">首页</el-col>
+            <el-col :span="6">日记</el-col>
+            <el-col :span="6">周报</el-col>
+            <el-col :span="6">关于我</el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'MyHead',
+    }
+</script>
+
+<style>
+    .head{
+        height: 3rem;
+        background: darkgray;
+        padding: 0.3rem;
+        text-align: center;
+        margin: -1rem -1rem 1rem -1rem;
+    }
+    .head-el{
+        width: 30%;
+        font-size: 1.5rem;
+        margin: 0.75rem 22rem 0.75rem 22rem;
+
+
+    }
+</style>
